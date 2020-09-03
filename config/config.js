aae75ed146abcebf705c259015e16aa42fcf8dc9
sunlidea <notifysunli@gmail.com>
2020-09-03 18:06:57 +0800

    Add sidebar

--- config/config.js
+++ config/config.js
@@ -10,8 +10,20 @@ export default {
         component: '../layout',
         routes: [
             {
-                path: 'helloworld',
-                component: './HelloWorld'
+                path: '/',
+                component: 'Helloworld',
+            },
+            {
+                path: '/helloworld',
+                component: 'Helloworld'
+            },
+            {
+                path: '/dashboard',
+                routes: [
+                    { path: '/dashboard/analysis', component: 'Dashboard/Analysis' },
+                    { path: '/dashboard/monitor', component: 'Dashboard/Monitor' },
+                    { path: '/dashboard/workplace', component: 'Dashboard/Workplace' }
+                ]
             },
         ]
     }],
